479ff71ac556a2fcb81172d7b1e856b38759070d
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-01-14 19:18:29 +0700

    Add bootstrap logging and resolve CA1848 correctly

--- Maliev.LeaveService.Api/Program.cs
+++ Maliev.LeaveService.Api/Program.cs
@@ -1,4 +1,3 @@
-#pragma warning disable CA1848 // For improved performance, use the LoggerMessage delegates
 using Microsoft.Extensions.Hosting;
 using Maliev.Aspire.ServiceDefaults;
 using Maliev.LeaveService.Infrastructure.Data;
@@ -16,7 +15,7 @@ var bootstrapLogger = loggerFactory.CreateLogger("Program");
 
 try
 {
-    bootstrapLogger.LogInformation("Starting Leave Service host");
+    Program.Log.StartingHost(bootstrapLogger, "Leave Service");
 
     var builder = WebApplication.CreateBuilder(args);
 
@@ -107,12 +106,12 @@ try
     app.MapControllers();
     app.MapApiDocumentation(servicePrefix: "leave");
 
-    logger.LogInformation("Leave service started successfully");
+    Program.Log.ServiceStarted(logger, "Leave Service");
     await app.RunAsync();
 }
 catch (Exception ex)
 {
-    bootstrapLogger.LogCritical(ex, "Leave Service host terminated unexpectedly during startup");
+    Program.Log.HostTerminated(bootstrapLogger, ex, "Leave Service");
     throw;
 }
 finally
@@ -120,4 +119,17 @@ finally
     loggerFactory.Dispose();
 }
 
-public partial class Program { }
+public partial class Program
+{
+    internal static partial class Log
+    {
+        [LoggerMessage(Level = LogLevel.Information, Message = "Starting {ServiceName} host")]
+        public static partial void StartingHost(ILogger logger, string serviceName);
+
+        [LoggerMessage(Level = LogLevel.Critical, Message = "{ServiceName} host terminated unexpectedly during startup")]
+        public static partial void HostTerminated(ILogger logger, Exception ex, string serviceName);
+
+        [LoggerMessage(Level = LogLevel.Information, Message = "{ServiceName} started successfully")]
+        public static partial void ServiceStarted(ILogger logger, string serviceName);
+    }
+}
